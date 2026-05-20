c78046a2147aefb27f4de02cb8ef8da555509b32
Al Amru Bil Maruf <alamrubilmaruf412@gmail.com>
2026-05-20 19:55:10 +0600

    implement isin generator

---
+++ java-case-study/src/main/java/com/solvians/showcase/ISINGenerator.java
@@ -0,0 +1,74 @@
+package com.solvians.showcase;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ISINGenerator {
+
+    private static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHANUMERIC_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int ISIN_LENGTH = 12;
+    private static final int UPPERCASE_LEN_LIMIT = 2;
+    private static final int ALPHANUMERIC_LEN_LIMIT = 9;
+
+    private final ThreadLocalRandom random;
+
+    public ISINGenerator() {
+        this.random = ThreadLocalRandom.current();
+    }
+
+    public ISINGenerator(ThreadLocalRandom random) {
+        this.random = random;
+    }
+
+    public String generateRandomISIN() {
+        StringBuilder isin = new StringBuilder(ISIN_LENGTH);
+
+        for (int i = 0; i < UPPERCASE_LEN_LIMIT; i++) {
+            isin.append(UPPERCASE_LETTERS.charAt(random.nextInt(UPPERCASE_LETTERS.length())));
+        }
+
+        for (int i = 0; i < ALPHANUMERIC_LEN_LIMIT; i++) {
+            isin.append(ALPHANUMERIC_CHARS.charAt(random.nextInt(ALPHANUMERIC_CHARS.length())));
+        }
+
+        isin.append(calculateCheckDigit(isin.toString()));
+
+        return isin.toString();
+    }
+
+    private String calculateCheckDigit(String isinBase) {
+        String isinNumeric = convertToNumericString(isinBase);
+
+        boolean multiply = true;
+        int sum = 0;
+        for (int i = isinNumeric.length() - 1; i >= 0; i--) {
+            int digit = isinNumeric.charAt(i) - '0';
+            if (!multiply) {
+                sum += digit;
+            } else {
+                digit *= 2;
+                sum += (digit / 10 + digit % 10);
+            }
+            multiply = !multiply;
+        }
+
+        int roundedUp = ((sum + 9) / 10) * 10;
+        return String.valueOf(roundedUp - sum);
+    }
+
+    private String convertToNumericString(String isinBase) {
+        StringBuilder numericString = new StringBuilder();
+
+        for (int i = 0; i < isinBase.length(); i++) {
+            char ch = isinBase.charAt(i);
+            if (Character.isDigit(ch)) {
+                numericString.append(ch);
+            } else {
+                numericString.append(ch - 'A' + 10);
+            }
+        }
+
+        return numericString.toString();
+    }
+
+}
